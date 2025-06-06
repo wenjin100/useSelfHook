0586512a3aa2cb416ff4591d797b9e0ded5e56c2
wenjin <wj1001001@qq.com>
2025-06-06 18:46:59 +0800

    github部署测试

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+// 为了解决 "类型“ImportMeta”上不存在属性“env”" 的问题，使用 process.env 替代 import.meta.env
+  base: process.env.MODE === 'production'
+  ? '/useSelfHook/' 
+  : '/',
 })
